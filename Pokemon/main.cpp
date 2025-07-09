cd8736172a155841e7e648c1c81bda222136c1ec
Pranav Bardiwale <iampbardiwale@gmail.com>
2025-07-09 12:51:26 +0530

    Giving the choice to with adding their name

--- Pokemon/main.cpp
+++ Pokemon/main.cpp
@@ -7,7 +7,27 @@ int main() {
     
     cin >> player_name;
 
-    cout << "Great Start " << player_name << ", looks like you have understood the main() function properly now!" << endl;
+    cout <<"Welcome to the Pokemon world " << player_name << "!,I am Professor Oak \n";
+    cout << "Choose your Pokemon\n";
+    cout << "1.Bulbasaur\n2.Charmander\n3.Squirtle\n";
+
+    int choice;
+    cin >> choice;
+
+    if (choice == 1) {
+        cout << "your mate is Bulbasaur\n";
+    }
+    else if (choice == 2) {
+        cout << "your mate is Charmander\n";
+    }
+    else if (choice == 3) {
+        cout << "Your mate is Squirtle\n";
+    }
+    else {
+        cout << "It's an invalid choice\n";
+    }
+
+    cout << "Ah, an excellent choice!\nBut beware, Trainer,this is only the beginning.\nYour journey is about to unfold.\nNow let’s see if you’ve got what it takes to keep going!\nGood luck, and remember… Choose wisely!";
 
     return 0;
 }
