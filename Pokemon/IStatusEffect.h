bd4d28bf59d48242fde69ad3dbe474b9a69e4cd2
Pranav Bardiwale <iampbardiwale@gmail.com>
2025-08-20 11:43:04 +0530

    InterFace Ready sir!

---
+++ Pokemon/IStatusEffect.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "Pokemon.h"
+class IStatusEffect
+{
+public:
+	virtual void applyEffect(Pokemon* target)=0;
+	virtual std::string getEffectName()=0;
+	virtual bool turnEndEffect(Pokemon* target)=0;
+
+	virtual void clearEffect(Pokemon* target)=0;
+
+	virtual ~IStatusEffect() = default;
+};
+
